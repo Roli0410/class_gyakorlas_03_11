a6257c054369c0aada74582c8dc5125eb23d32bb
Roli0410 <muller.gabor.roland@diak.szbi-pg.hu>
2026-03-11 08:33:20 +0100

    macska_class

---
+++ emlos.py
@@ -0,0 +1,17 @@
+from allat import Allat
+
+class Emlos(Allat):
+    def __init__(self, nev_, faj_, eletkor_, elohely_,  szorzet_szine_):
+        super().__init__(nev_, faj_, eletkor_, elohely_, "közepes")
+        self.szorzet_szine = szorzet_szine_
+
+    def __str__(self):
+        return f"{self.nev} {self.faj} {self.eletkor} éves, élőhelye {self.elohely}, mérete: {self.meret}, szőrzete: {self.szorzet_szine}"
+
+
+class Macska(Emlos):
+    def __init__(self, nev_, eletkor_, elohely_, szorzet_szine_):
+        super().__init__(nev_, "macska", eletkor_, elohely_, szorzet_szine_)
+
+    def dorombol(self):
+        print("{self.nev} éppen dorombol")
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from allat import Allat
+from emlos import Emlos, Macska
+
 
 
 allat1 = Allat("Bodri", "kutya", 5, "kert", "közepes")
@@ -6,3 +8,15 @@ allat2 = Allat("Cirmi", "macska", 2, "ház", "közepes")
 
 print(allat1)
 print(allat2)
+
+print("\nEMLŐSÖK\n")
+
+emlos1 = Emlos("Morzsi", "kutya", 5, "kert", "barna")
+emlos2 = Emlos("Cicó", "macska", 3, "huzz", "cirmos")
+
+print(emlos1)
+print(emlos2)
+
+
+macska1 = Macska("Hubert", 4, "ház", "fehér")
+print(macska1)
